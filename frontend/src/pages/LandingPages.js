5e64d3dd62778bddb4a84c25b3f941ab82e70d9f
Rino Satya putra <rinosatyaputra.id@gmail.com>
2020-06-23 13:55:26 +0700

    Source css bootstrap & js

--- frontend/src/pages/LandingPages.js
+++ frontend/src/pages/LandingPages.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
-
+import Header from "../parts/Header";
+import LandingPages from "../data/LandingBlibli.json";
 export default class LandingPages extends Component {
 	render() {
-		return <div></div>;
+		return (
+			<>
+				<Header data={LandingPages.Header} />
+			</>
+		);
 	}
 }
