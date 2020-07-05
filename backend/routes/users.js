0151318d47c193f2ded0cdc1cd1d67341f46e490
Rino Satya putra <rinosatyaputra.id@gmail.com>
2020-07-05 17:14:02 +0700

    Login and Register Added

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,37 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
+var Users = require("../model/users");
 /* GET users listing. */
-router.post('/v1/api/blibli/register  ', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.post("/v1/api/blibli/login", function (req, res) {
+	const { body } = req;
+
+	const hash = crypto
+		.createHmac("sha256", secret)
+		.update(body.password)
+		.digest("hex");
+
+	Users.getData({ ...body, password: hash }, function (err, result, fields) {
+		if (err) throw err;
+		req.session.email = result.email;
+		req.session.password = result.password;
+		// return res.send({ err, result, fields });
+		req.session.save(() => res.redirect("/v1/api/blibli"));
+	});
+});
+
+router.post("/v1/api/blibli/register", function (req, res) {
+	const { body } = req;
+
+	const hash = crypto
+		.createHmac("sha256", secret)
+		.update(body.password)
+		.digest("hex");
+
+	Users.create({ ...body, password: hash }, (err, result) => {
+		if (err) throw err;
+		res.redirect("/");
+	});
 });
 
 module.exports = router;
